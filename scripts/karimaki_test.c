34fe809cfd211b2c8e834f1a27a5ea34e0326f46
Konstantin Neureither <info@kneureither.de>
2020-05-22 15:43:20 +0200

    fixed mistakes

--- scripts/karimaki_test.c
+++ scripts/karimaki_test.c
@@ -1,33 +1,24 @@
-//#include <string>
-//#include <fstream>
-//#include <iostream>
 #include <stdlib.h>
 #include <math.h>
 #include <stdio.h>
 
 #include "../3rdparty/karimaki/karimaki_hit.c"
-#include "../3rdparty/karimaki/karimaki.h"
-#include "../util/utility_functions.h"
-#include "../util/custom_types.h"
-
-using std::cout;
-using std::endl;
 
 int main(int argc, char *argv[]) {
 
     int npoints = 4;
-    double xp[85.8653, 71.3723,-36.469,-49.4697];
-    double yp[-2.68301,-9.67875,-62.8534, -69.3326];
-    double zp[-493.95, -496.46, -514.88, -517.09];
-    double tres[1.0,1.0,1.0,1.0];
-    double zres[1.0,1.0,1.0,1.0];
-    double rres[1.0,1.0,1.0,1.0];
+    double xp[4] = {85.8653, 71.3723,-36.469,-49.4697};
+    double yp[4] = {-2.68301,-9.67875,-62.8534, -69.3326};
+    double zp[4] = {-493.95, -496.46, -514.88, -517.09};
+    double tres[4] = {1.0,1.0,1.0,1.0};
+    double zres[4] = {1.0,1.0,1.0,1.0};
+    double rres[4] = {1.0,1.0,1.0,1.0};
 
     KARITRACK karidata;
 
     karimaki_hit(npoints, xp, yp, zp, tres, zres, rres);
 
-    cout << "success!" << endl;
-    cout << "kari.phi=" << kari.phi << endl;
+    printf("success!");
+    printf("kari.phi=%4.2f", kari.phi);
 
 }
