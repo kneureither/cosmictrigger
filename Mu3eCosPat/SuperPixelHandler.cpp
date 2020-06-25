1410195ab64c305eebfcf30d74d3ea916b4205c2
Konstantin Neureither <info@kneureither.de>
2020-06-25 12:36:17 +0200

    created source files

---
+++ Mu3eCosPat/SuperPixelHandler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Konstantin Neureither on 25.06.20.
+//
+
+#include "SuperPixelHandler.h"
---
+++ Mu3eCosPat/SuperPixelHandler.h
@@ -0,0 +1,16 @@
+//
+// Created by Konstantin Neureither on 25.06.20.
+//
+
+#ifndef COSMICTRIGGER_SUPERPIXELHANDLER_H
+#define COSMICTRIGGER_SUPERPIXELHANDLER_H
+
+
+
+class SuperPixelHandler {
+
+};
+
+
+
+#endif //COSMICTRIGGER_SUPERPIXELHANDLER_H
