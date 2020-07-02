8e99cc8e00bd5e75d27b2e18af673c9f42c3d3c8
Konstantin Neureither <info@kneureither.de>
2020-07-02 11:40:57 +0200

    improved longitudinal fit implementation (moved to first hit ref frame)

--- 3rdparty/karimaki/karimaki_hit.cpp
+++ 3rdparty/karimaki/karimaki_hit.cpp
@@ -60,11 +60,9 @@ int karimaki_hit(KARITRACK &karires, int npoints, double xp[MAXLAYER], double yp
   float rad;
   float r_hit;
   float phi_hit;
-  float arg;
   float alpha;
   float d_hit;
-  float alph;
-  float theta,r3d;
+  float theta,r3d, z_off;
 
   /* number hits */
   NumConstr=0;
@@ -97,57 +95,51 @@ int karimaki_hit(KARITRACK &karires, int npoints, double xp[MAXLAYER], double yp
 
   // radius parameter
   rad=1.0/TPar[0];
-
 //  printf("karimaki_hit result: rad=%f, chi2=%f \n",rad,TCh2dF );
 
-  //TODO calculate hit in 0,0 plane correctly
 
-  //calculate approx for hit in x,z plane
-  float x1 = ConstrX[1];
-  float x2 = ConstrX[npoints-2];
-  float y1 = ConstrY[1];
-  float y2 = ConstrY[npoints-2];
-  float t = - y1 / (y2 - y1);
-  float x0 = x1 + t * (x2 - x1);
-  float y0 = y1 + t * (y2 - y1);
+//// Do the helix fit in 3rd dimension
 
   for (i=0;i<npoints;i++) {
-//      d_hit = sqrt(pow(ConstrX[i] - ConstrX[0], 2) + pow(ConstrY[i] - ConstrX[0], 2));
-//      alph = asin(d_hit / rad);
+      //transform to first hit at (0,0) frame
+      ConstrX[i]=xp[i] - xp[0];
+      ConstrY[i]=yp[i] - yp[0];
 
-    r_hit=sqrt(pow(ConstrX[i] - x0 ,2)+pow(ConstrY[i] - y0,2));
-    r_hit = (ConstrY[i] < 0 ? r_hit : -r_hit);
-    phi_hit=atan2(ConstrY[NumConstr],ConstrX[NumConstr]);
-    arg=0.5*r_hit/rad;
+      //get arc length in 1st ref frame
+      d_hit = sqrt(pow(ConstrX[i], 2) + pow(ConstrY[i], 2));
+      alpha = 2*asin(0.5*d_hit / rad);
 
-// hit reached ? -- OBSOLETE! TODO
-    if (fabs(arg)<1) {
-      alpha=(arg);
-//      ConstrS[i] = rad*alph; //corrected ConstrS
-      ConstrS[i]=2*rad*alpha;  // arc length in 2d
-//      ConstrS[i]=r_hit;  // arc length in 2d for small arg
+      phi_hit=atan2(ConstrY[NumConstr],ConstrX[NumConstr]);
+
+      ConstrS[i] = rad*alpha; //corrected ConstrS
       ZWeight[i]=1.0/zres[i];
-      if(DEBUG_CONSOLE_LOG) printf("\tZweight as in call (RMS)=%f theta from ms=%f \t alpha=%f arg=%f Constr[%d]=%f\n", ZWeight[i], thetas[i], alpha, arg, i, ConstrS[i]);
-    } else {
-      ConstrS[i]=0.0;
-      ZWeight[i]=0.0;  // standard weight
-      if(DEBUG_CONSOLE_LOG) printf("\tSomething went wrong!\n");
-    }
+
+// // hit reached ? -- OBSOLETE! TODO
+//    if (fabs(arg)<1) {
+//      ConstrS[i] = rad*alpha; //corrected ConstrS
+// //        ConstrS[i]=2*rad*alpha;  // arc length in 2d
+//        ZWeight[i]=1.0/zres[i];
+//        if(DEBUG_CONSOLE_LOG) printf("\tZweight as in call (RMS)=%f theta from ms=%f \t alpha=%f arg=%f Constr[%d]=%f\n", ZWeight[i], thetas[i], alpha, arg, i, ConstrS[i]);
+//    } else {
+//      ConstrS[i]=0.0;
+//      ZWeight[i]=0.0;  // standard weight
+//      if(DEBUG_CONSOLE_LOG) printf("\tSomething went wrong!\n");
+//    }
   }
+
+
   // first 2d track fit in longitudinal plane
   fitszw (ConstrS,ConstrZ,ZWeight,NumConstr,ZPar,&ZCh2dF);
   if(DEBUG_CONSOLE_LOG) printf("\tKarimaki longitudinal track parameter: %f %f  chi2norm=%f,\n",ZPar[0],ZPar[1],ZCh2dF);
 
-  // redo weight calculation of used hits
+  // redo weight calculation of used hits with fitted parameters
   theta=atan2(1.0,ZPar[1]);
-  if(DEBUG_CONSOLE_LOG)  printf("\ttheta from kari=%f\n", theta);
 
-  for (i=0;i<npoints;i++) {
-    if (ZWeight[i]>0) {
-        theta = thetas[i];
-      ZWeight[i]=1.0/abs(zres[i]*sin(theta));
-      if(DEBUG_CONSOLE_LOG) printf("\ttheta=%f sin(theta)=%f new weight = %f\n", theta, sin(theta), ZWeight[i]);
-    }
+  for (i=0;i<npoints;i++){
+      if (ZWeight[i]>0) {
+          theta = thetas[i];
+          ZWeight[i]=1.0/abs(zres[i]*sin(theta));
+      }
   }
 
   // redo fit with correct weights
@@ -157,13 +149,15 @@ int karimaki_hit(KARITRACK &karires, int npoints, double xp[MAXLAYER], double yp
   // store results
   theta=atan2(1.0,ZPar[1]);
   r3d=1.0/TPar[0]/sin(theta);
+  z_off = sqrt(pow(xp[0], 2) + pow(yp[0], 2)) / tan(theta);
+
 
     //karires.rad=TPar[0];
     karires.rad=rad;
     karires.r3d=r3d;
     karires.dca=TPar[1];
     karires.phi=TPar[2];
-    karires.z0=ZPar[0];
+    karires.z0=ZPar[0] + z_off;
     karires.theta=theta;
     karires.tchi2n=TCh2dF;
     karires.zchi2n=ZCh2dF;
