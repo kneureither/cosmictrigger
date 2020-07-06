0eb02a530753f8442023b8edc2f241a6ba00e128
Konstantin Neureither <info@kneureither.de>
2020-07-06 19:39:54 +0200

    Maxlayer update

--- karimaki/karimakiHelixfit.h
+++ karimaki/karimakiHelixfit.h
@@ -16,14 +16,15 @@ typedef struct KariFit
   float zchi2n;
 } KARITRACK;
 
-int karimakiHelixfit(KariFit &karires, int npoints, double xp[16], double yp[16], double zp[16], double phip[16], double thetas[16], double tres[16], double zres[16], double rres[16]);
+int karimakiHelixfit(KariFit &karires, int npoints, double xp[MAXLAYER], double yp[MAXLAYER], double zp[MAXLAYER],
+        double phip[MAXLAYER], double thetas[MAXLAYER], double tres[MAXLAYER], double zres[MAXLAYER], double rres[MAXLAYER]);
 
 template <typename T>
 static int sign(const T val) {
     return (T(0) < val) - (val < T(0));
 }
 
-void swapKariMomentum(KariFit &kari, float rec_r, int &count) {
+static void swapKariMomentum(KariFit &kari, float rec_r, int &count) {
     if(sign(kari.rad) != sign(rec_r)) {
         kari.rad = -kari.rad;
         kari.dca = -kari.dca;
