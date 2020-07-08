20909f4de7c95908acf86ccf88f3d71577e5e664
Konstantin Neureither <info@kneureither.de>
2020-07-08 17:41:16 +0200

    fixes

--- util/rootData.h
+++ util/rootData.h
@@ -8,6 +8,7 @@
 #include <math.h>
 #include "../karimaki/karimakiHelixfit.h"
 #include "basicDefines.h"
+#include "SegsTreeRead.h"
 
 template <typename T>
 static unsigned int get_layer(T sid) {
@@ -27,13 +28,13 @@ static unsigned int get_layer(T sid) {
 
 static int combineHits(std::vector<double> &xp, std::vector<double> &yp, std::vector<double> &zp,
                 std::vector<int> &sids, std::vector<double> &phi_tracks, std::vector<double> &thetas, int nhits, int ntriplets,
-                float (&x00)[TRIPLET_HIT_ARRAY_LENGTH], float (&x10)[TRIPLET_HIT_ARRAY_LENGTH],
+                float (&x00)[TRIPLET_HIT_ARRAY_LENGTH], float (&x10)[TRIPLET_HIT_ARRAY_LENGTH], float (&x11)[TRIPLET_HIT_ARRAY_LENGTH],
                 float (&x01)[TRIPLET_HIT_ARRAY_LENGTH], float (&x20)[TRIPLET_HIT_ARRAY_LENGTH], float (&x21)[TRIPLET_HIT_ARRAY_LENGTH],
-                float (&y00)[TRIPLET_HIT_ARRAY_LENGTH], float (&y10)[TRIPLET_HIT_ARRAY_LENGTH],
+                float (&y00)[TRIPLET_HIT_ARRAY_LENGTH], float (&y10)[TRIPLET_HIT_ARRAY_LENGTH], float (&y11)[TRIPLET_HIT_ARRAY_LENGTH],
                 float (&y01)[TRIPLET_HIT_ARRAY_LENGTH], float (&y20)[TRIPLET_HIT_ARRAY_LENGTH], float (&y21)[TRIPLET_HIT_ARRAY_LENGTH],
-                float (&z00)[TRIPLET_HIT_ARRAY_LENGTH], float (&z10)[TRIPLET_HIT_ARRAY_LENGTH],
+                float (&z00)[TRIPLET_HIT_ARRAY_LENGTH], float (&z10)[TRIPLET_HIT_ARRAY_LENGTH], float (&z11)[TRIPLET_HIT_ARRAY_LENGTH],
                 float (&z01)[TRIPLET_HIT_ARRAY_LENGTH], float (&z20)[TRIPLET_HIT_ARRAY_LENGTH], float (&z21)[TRIPLET_HIT_ARRAY_LENGTH],
-                float (&sid00)[TRIPLET_HIT_ARRAY_LENGTH], float (&sid10)[TRIPLET_HIT_ARRAY_LENGTH],
+                float (&sid00)[TRIPLET_HIT_ARRAY_LENGTH], float (&sid10)[TRIPLET_HIT_ARRAY_LENGTH], float (&sid11)[TRIPLET_HIT_ARRAY_LENGTH],
                 float (&sid01)[TRIPLET_HIT_ARRAY_LENGTH], float (&sid20)[TRIPLET_HIT_ARRAY_LENGTH], float (&sid21)[TRIPLET_HIT_ARRAY_LENGTH],
                 float (&tan01)[TRIPLET_HIT_ARRAY_LENGTH], float (&tan12)[TRIPLET_HIT_ARRAY_LENGTH],
                 float (&lam01)[TRIPLET_HIT_ARRAY_LENGTH], float (&lam12)[TRIPLET_HIT_ARRAY_LENGTH]) {
@@ -72,18 +73,22 @@ static int combineHits(std::vector<double> &xp, std::vector<double> &yp, std::ve
                 yp.push_back(y01[i]);
                 zp.push_back(z01[i]);
                 sids.push_back((int) sid01[i]);
+                phi_tracks.push_back(tan01[(i == 0 ? 0 : i-1)]);
+                thetas.push_back(PI / 2 - lam01[(i == 0 ? 0 : i-1)]);
                 ncomhits++;
                 printf("Hit found in x01\n");
             }
 
-            it = std::find(xp.begin(), xp.end(), x10[i]);
+            it = std::find(xp.begin(), xp.end(), x11[i]);
             if(it == xp.end()) {
-                xp.push_back(x10[i]);
-                yp.push_back(y10[i]);
-                zp.push_back(z10[i]);
-                sids.push_back((int) sid10[i]);
+                xp.push_back(x11[i]);
+                yp.push_back(y11[i]);
+                zp.push_back(z11[i]);
+                sids.push_back((int) sid11[i]);
+                phi_tracks.push_back(tan01[i]);
+                thetas.push_back(PI / 2 - lam01[i]);
                 ncomhits++;
-                printf("Hit found in x10\n");
+                printf("Hit found in x11\n");
             }
 
             it = std::find(xp.begin(), xp.end(), x21[i]);
@@ -92,6 +97,79 @@ static int combineHits(std::vector<double> &xp, std::vector<double> &yp, std::ve
                 yp.push_back(y21[i]);
                 zp.push_back(z21[i]);
                 sids.push_back((int) sid21[i]);
+                phi_tracks.push_back(tan12[i]);
+                thetas.push_back(PI / 2 - lam12[i]);
+                ncomhits++;
+                printf("Hit found in x21\n");
+            }
+        }
+    }
+    return ncomhits;
+}
+
+static int combineHits(std::vector<double> &xp, std::vector<double> &yp, std::vector<double> &zp,
+                       std::vector<int> &sids, std::vector<double> &phi_tracks, std::vector<double> &thetas,
+                       SegsTreeRead &Segs, bool ADD_REDUNDANT_HITS) {
+
+    //These two loops add the basic ntriplets+2 hits
+    for(int i=0; i<Segs.rec_ntriplet; i++) {
+        xp.push_back(Segs.x00[i]);
+        yp.push_back(Segs.y00[i]);
+        zp.push_back(Segs.z00[i]);
+        sids.push_back((int) Segs.sid00[i]);
+        phi_tracks.push_back(Segs.rec_tan01[(i == 0 ? 0 : i-1)]);
+        thetas.push_back(PI / 2 - Segs.rec_lam01[(i == 0 ? 0 : i-1)]);
+    }
+    for(int i=Segs.rec_ntriplet-2; i<Segs.rec_ntriplet; i++) {
+        xp.push_back(Segs.x20[i]);
+        yp.push_back(Segs.y20[i]);
+        zp.push_back(Segs.z20[i]);
+        sids.push_back((int) Segs.sid20[i]);
+        phi_tracks.push_back(Segs.rec_tan12[i]);
+        thetas.push_back(PI / 2 - Segs.rec_lam12[i]);
+    }
+    int ncomhits = Segs.rec_ntriplet+2;
+
+    //when this is true, all hits in the segments are searched for additional duplicate hits
+//    bool ADD_REDUNDANT_HITS = false;
+    //TODO extract tan and theta for these hits also
+
+    if((ADD_REDUNDANT_HITS ? Segs.rec_nhit > Segs.rec_ntriplet+2 : false)) {
+        printf("ntriplets = %d, nhits = %d\n", Segs.rec_ntriplet, Segs.rec_nhit);
+        std::vector<double>::iterator it;
+        for(int i=0; i<Segs.rec_ntriplet; i++) {
+            it = std::find(xp.begin(), xp.end(), Segs.x01[i]);
+            if(it == xp.end()) {
+                xp.push_back(Segs.x01[i]);
+                yp.push_back(Segs.y01[i]);
+                zp.push_back(Segs.z01[i]);
+                sids.push_back((int) Segs.sid01[i]);
+                phi_tracks.push_back(Segs.rec_tan01[(i == 0 ? 0 : i-1)]);
+                thetas.push_back(PI / 2 - Segs.rec_lam01[(i == 0 ? 0 : i-1)]);
+                ncomhits++;
+                printf("Hit found in x01\n");
+            }
+
+            it = std::find(xp.begin(), xp.end(), Segs.x11[i]);
+            if(it == xp.end()) {
+                xp.push_back(Segs.x11[i]);
+                yp.push_back(Segs.y11[i]);
+                zp.push_back(Segs.z11[i]);
+                sids.push_back((int) Segs.sid11[i]);
+                phi_tracks.push_back(Segs.rec_tan01[i]);
+                thetas.push_back(PI / 2 - Segs.rec_lam01[i]);
+                ncomhits++;
+                printf("Hit found in x11\n");
+            }
+
+            it = std::find(xp.begin(), xp.end(), Segs.x21[i]);
+            if(it == xp.end()) {
+                xp.push_back(Segs.x21[i]);
+                yp.push_back(Segs.y21[i]);
+                zp.push_back(Segs.z21[i]);
+                sids.push_back((int) Segs.sid21[i]);
+                phi_tracks.push_back(Segs.rec_tan12[i]);
+                thetas.push_back(PI / 2 - Segs.rec_lam12[i]);
                 ncomhits++;
                 printf("Hit found in x21\n");
             }
@@ -131,19 +209,7 @@ static int combineBasicHits(std::vector<double> &xp, std::vector<double> &yp, st
     return ncomhits;
 }
 
-static void correctKariDirection(KariFit &kari) {
-
-    if(kari.phi > 0) {
-        //printf("\tKARI CORRECT OLD: \t rad %f \tr3d %f phi %f \t theta %f\n", kari.rad, kari.r3d, kari.phi, kari.theta);
-        kari.rad = -kari.rad;
-        kari.r3d = -kari.r3d;
-        kari.phi = kari.phi - PI; //this is save as phi>0
-//        kari.theta = PI - kari.theta;
-        //printf("\tKARI CORRECT NEW: \t rad %f \tr3d %f phi %f \t theta %f\n", kari.rad, kari.r3d, kari.phi, kari.theta);
-    }
-}
-
-int counthitlayers(int nhits, float (&sid00)[TRIPLET_HIT_ARRAY_LENGTH], float (&sid11)[TRIPLET_HIT_ARRAY_LENGTH]) {
+static int counthitlayers(int nhits, float (&sid00)[TRIPLET_HIT_ARRAY_LENGTH], float (&sid11)[TRIPLET_HIT_ARRAY_LENGTH]) {
     int layerhits[4] = {0,0,0,0};
 
     for(int i=0; i<nhits-2; i++) {
@@ -159,7 +225,7 @@ int counthitlayers(int nhits, float (&sid00)[TRIPLET_HIT_ARRAY_LENGTH], float (&
     return layerhits[0] + layerhits[1] + layerhits[2] + layerhits[3];
 }
 
-int get_charge_from_type(int type) {
+static int get_charge_from_type(int type) {
     if (type == 4) {
         //mu-
         return 1;
