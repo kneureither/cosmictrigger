3ce4fdc2f6d26cb234bc2275543653a7fa29f9fe
Konstantin Neureither <info@kneureither.de>
2020-10-14 17:50:49 +0200

    new file name adapted

--- Mu3eCTBgAna/inc/bgeval.h
+++ Mu3eCTBgAna/inc/bgeval.h
@@ -12,4 +12,12 @@ std::string get_bgevalfile(int bgevents, int run, int dataset, int mode, int wbi
         "_dataset" + get_string(dataset) + "_" + getfileidtag(mode, wbins, zbins) + "_plots.root";
 }
 
+std::string get_bgevalfile(int bg_events, int cosmic_eff_events, int bg_run, int cosmic_eff_dataset, int dataset, int mode, int wbins, int zbins) {
+    return "CosmicTBGAna_bkgEv" + get_padded_string(bg_events, 6, '0') +
+           "_cosEv" + get_padded_string(cosmic_eff_events, 6, '0') +
+           "_bkgrun_" + get_padded_string(bg_run, 6, '0') +
+           "_cosdst_" + get_padded_string(cosmic_eff_dataset, 6, '0') +
+           "_dataset" + get_string(dataset) + "_" + getfileidtag(mode, wbins, zbins) + "_plots.root";
+}
+
 #endif //COSMICTRIGGER_BGEVAL_H
